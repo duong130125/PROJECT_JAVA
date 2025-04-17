029ec31285b7d5be83faa2070dc4dc187ebb15ce
Duong <nguyendinhduong2005yt@gmail.com>
2025-04-17 15:31:37 +0700

    task3

--- src/console.sql
+++ src/console.sql
@@ -51,3 +51,51 @@ CREATE TABLE invoice_details
     FOREIGN KEY (invoice_id) REFERENCES invoice (id),
     FOREIGN KEY (product_id) REFERENCES product (id)
 );
+
+insert into admin(username, password)
+values
+    ('duongdinh', '13012005');
+-- Đăng nhập Admin
+DELIMITER //
+CREATE PROCEDURE PROC_ADMIN_LOGIN(IN user_name VARCHAR(50), IN pass VARCHAR(50))
+BEGIN
+    SELECT * FROM admin WHERE username = user_name AND password = pass;
+END //
+DELIMITER ;
+
+-- Thêm sản phẩm
+DELIMITER //
+CREATE PROCEDURE PROC_INSERT_PRODUCT(IN p_name VARCHAR(100), IN p_brand VARCHAR(50), IN p_price DECIMAL(12,2), IN p_stock INT)
+BEGIN
+    INSERT INTO product(name, brand, price, stock) VALUES (p_name, p_brand, p_price, p_stock);
+END;
+//
+DELIMITER ;
+
+-- Hiển thị danh sách
+DELIMITER //
+CREATE PROCEDURE PROC_GET_ALL_PRODUCT()
+BEGIN
+    SELECT * FROM product;
+END;
+//
+DELIMITER ;
+
+-- Cập nhật sản phẩm
+DELIMITER //
+CREATE PROCEDURE PROC_UPDATE_PRODUCT(IN p_id INT, IN p_name VARCHAR(100), IN p_brand VARCHAR(50), IN p_price DECIMAL(12,2), IN p_stock INT)
+BEGIN
+    UPDATE product SET name = p_name, brand = p_brand, price = p_price, stock = p_stock WHERE id = p_id;
+END;
+//
+DELIMITER ;
+
+-- Xóa sản phẩm
+DELIMITER //
+CREATE PROCEDURE PROC_DELETE_PRODUCT(IN p_id INT)
+BEGIN
+    DELETE FROM product WHERE id = p_id;
+END;
+//
+DELIMITER ;
+
